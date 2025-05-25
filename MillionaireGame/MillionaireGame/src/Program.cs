c79a332ac33ca247f64e5768adb1706443629ccd
MarionVayt <vusatko@gmail.com>
2025-05-25 19:38:13 +0300

    Optimized Code.

--- MillionaireGame/MillionaireGame/src/Program.cs
+++ MillionaireGame/MillionaireGame/src/Program.cs
@@ -1,95 +1,91 @@
 ﻿class Program
 {
     static void Main()
-{
-    Console.OutputEncoding = System.Text.Encoding.UTF8;
-    Console.InputEncoding = System.Text.Encoding.UTF8;
-    string recordFile = "Data/Records.txt";
-
-    bool playAgain = true;
-
-    while (playAgain)
     {
-        Console.Clear();
-        Logo.StartLogo();
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.InputEncoding = System.Text.Encoding.UTF8;
+        string recordFile = "Data/Records.txt";
 
-        string playerName = Utils.GetValidUsername();
-        Console.Clear();
-        Console.ForegroundColor = ConsoleColor.DarkMagenta;
-        Console.WriteLine($"Вітаю, {playerName}! Почнемо гру.");
-        Console.ResetColor();
+        bool playAgain = true;
 
-        CategoryManager categoryManager = new CategoryManager();
-        int score = 0;
-        bool gameover = false;
-        bool gameContinue = true;
-
-        while (categoryManager.HasCategories && !gameover)
+        while (playAgain)
         {
-            categoryManager.ShowCategories();
-            int choose = Utils.GetValidCategoryChoice(categoryManager.CategoryCount());
+            Console.Clear();
+            Logo.StartLogo();
 
-            string selectedFile = categoryManager.ChooseCategory(choose);
-            categoryManager.ShowCategory(selectedFile);
+            string playerName = Utils.GetValidUsername();
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine($"Вітаю, {playerName}! Почнемо гру.");
+            Console.ResetColor();
 
-            List<Question> questions = Question.LoadQuestions(selectedFile);
-            List<string> hints = new List<string> { "50/50", "Допомога друга", "Допомога залу" };
+            CategoryManager categoryManager = new CategoryManager();
+            int score = 0;
+            bool gameover = false;
+            bool gameContinue = true;
 
-            foreach (var question in questions)
+            while (categoryManager.HasCategories && !gameover)
             {
-                if (!PlayManager.PlayQuestion(question, hints, ref score))
-                {
-                    RecordManager.SaveRecord(playerName, score, recordFile);
-                    gameover = true;
-                    break;
-                }
-                else
-                {
-                    if (!Utils.AskForContinueCategory(ref score))
-                    {
-                        gameContinue = false;
-                        break;
-                    }
-                }
-            }
+                categoryManager.ShowCategories();
+                int choose = Utils.GetValidCategoryChoice(categoryManager.CategoryCount());
 
-            if (!gameover && gameContinue)
-            {
-                categoryManager.RemoveCategory(selectedFile);
+                string selectedFile = categoryManager.ChooseCategory(choose);
+                categoryManager.ShowCategory(selectedFile);
 
-                if (categoryManager.HasCategories)
+                List<Question> questions = Question.LoadQuestions(selectedFile);
+                List<string> hints = new List<string> { "50/50", "Допомога друга", "Допомога залу" };
+
+                foreach (var question in questions)
                 {
-                    if (!Utils.AskForContinueNextCategory(ref score))
+                    if (!PlayManager.PlayQuestion(question, hints, ref score))
                     {
+                        RecordManager.SaveRecord(playerName, score, recordFile);
                         gameover = true;
                         break;
                     }
+                    else
+                    {
+                        if (!Utils.AskForContinueCategory(ref score))
+                        {
+                            gameContinue = false;
+                            break;
+                        }
+                    }
+                }
+                if (!gameContinue)
+                {
+                    gameover = true;
+                    break;
+                }
+
+                if (!gameover && gameContinue)
+                {
+                    categoryManager.RemoveCategory(selectedFile);
+
+                    if (categoryManager.HasCategories)
+                    {
+                        if (!Utils.AskForContinueNextCategory(ref score))
+                        {
+                            gameover = true;
+                            break;
+                        }
+                    }
                 }
             }
-        }
-
-        if (!gameover)
-        {
+        
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine();
-            Console.WriteLine("\n🎉 Ви пройшли всі доступні категорії!");
+            if (!gameover)
+                Console.WriteLine("\n🎉 Ви пройшли всі доступні категорії!");
             Console.WriteLine($"Ваш фінальний баланс: {score}$.");
             Console.ResetColor();
+
             RecordManager.SaveRecord(playerName, score, recordFile);
+
+            playAgain = Utils.AskForRestart(recordFile);
+
         }
 
-        if (gameover)
-        {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine();
-            Console.WriteLine($"Ваш фінальний баланс: {score}$.");
-            Console.ResetColor();
-            RecordManager.SaveRecord(playerName, score, recordFile);
-        }
-
-        playAgain = Utils.AskForRestart(recordFile);
+        Logo.FinishLogo();
     }
-
-    Logo.FinishLogo();
-}
 }
